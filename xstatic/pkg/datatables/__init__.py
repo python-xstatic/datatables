4fc00cd901a5e7edb01c48c3a7b67b52e23a11df
David Moreau-Simard <dms@redhat.com>
2017-04-22 19:35:41 -0400

    Update to upstream 1.10.15

--- xstatic/pkg/datatables/__init__.py
+++ xstatic/pkg/datatables/__init__.py
@@ -11,9 +11,9 @@ NAME = __name__.split('.')[-1] # package name (e.g. 'foo' or 'foo_bar')
                                # please use a all-lowercase valid python
                                # package name
 
-VERSION = '1.9.4' # version of the packaged files, please use the upstream
+VERSION = '1.10.15' # version of the packaged files, please use the upstream
                   # version number
-BUILD = '2' # our package build number, so we can release new builds
+BUILD = '1' # our package build number, so we can release new builds
              # with fixes for xstatic stuff.
 PACKAGE_VERSION = VERSION + '.' + BUILD # version used for PyPi
 
@@ -24,8 +24,8 @@ CLASSIFIERS = []
 KEYWORDS = '%s xstatic' % NAME
 
 # XStatic-* package maintainer:
-MAINTAINER = 'Cheer Xiao'
-MAINTAINER_EMAIL = 'xiaqqaix@gmail.com'
+MAINTAINER = 'David Moreau Simard'
+MAINTAINER_EMAIL = 'dmsimard@redhat.com'
 
 # this refers to the project homepage of the stuff we packaged:
 HOMEPAGE = 'http://www.datatables.net'
@@ -46,7 +46,6 @@ LOCATIONS = {
     # your relative path/filenames should usually be without version
     # information, because either the base dir/url is exactly for this
     # version or the mapping will care for accessing this version.
-    ('microsoft', 'http'): 'http://ajax.aspnetcdn.com/ajax/jquery.dataTables/%s' % VERSION,
-    ('microsoft', 'https'): 'https://ajax.aspnetcdn.com/ajax/jquery.dataTables/%s' % VERSION,
+    ('datatables', 'http'): 'http://cdn.datatables.net/%s' % VERSION,
+    ('datatables', 'https'): 'https://cdn.datatables.net/%s' % VERSION
 }
-
